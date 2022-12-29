653fd64612f1a03c6f793b7b3bfd625cd7c4b064
Chamath Jayarathne <chamathsocial@gmail.com>
2022-12-29 12:39:28 -0500

    feat[Cards]: Style cards

--- src/components/id-card/id-card.component.jsx
+++ src/components/id-card/id-card.component.jsx
@@ -1,13 +1,22 @@
 import './id-card.styles.scss';
 
 export const IdCard = ({element}) => {
-    console.log(element.login.uuid);
+    console.log(element);
 
     return(
         <div className="IdCard-container">
-            <img src={element.picture.medium} alt={element.name.last} />
+            <img src={element.picture.large} alt={element.name.last} />
             <div className="IdCard__name">
                 {element.name.title} {element.name.first} {element.name.last}
+            </div>
+            <div className="IdCard__age">
+                <span>Age: </span>{element.dob.age}
+            </div>
+            <div className="IdCard__location">
+                <span>Lives in: </span>{element.location.city}, {element.location.country}
+            </div>
+            <div className="IdCard__email">
+                <span>Email: </span>{element.email}
             </div>
         </div>
     )
