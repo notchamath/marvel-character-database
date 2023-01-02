28c7add7b4cd5dd6517678a467f96303105bd538
Chamath Jayarathne <chamathsocial@gmail.com>
2023-01-02 18:27:34 -0500

    feat: pull data from API on search

--- src/components/searchbar/searchbar.component.jsx
+++ src/components/searchbar/searchbar.component.jsx
@@ -6,20 +6,43 @@ import useSearchDb from '../../custom-hooks/useSearchDb';
 import './searchbar.styles.scss';
 
 export const Searchbar = () => {
-    const [query, setQuery] = useState('spider-man');
-    const [offset, setOffset] = useState(0);
 
-    const handleSearch = (e) => {
-        setQuery(e.target.value);
-        setOffset(0);   //need to change
+    const requestLimit = 10;
+    const [value, setValue] = useState('');
+    
+
+    const loadData = async (query) => {
+
+        try{
+            const url = `https://gateway.marvel.com/v1/public/characters?limit=${requestLimit}&nameStartsWith=${query}&ts=${process.env.REACT_APP_TS}&apikey=${process.env.REACT_APP_PUBLIC_KEY}&hash=${process.env.REACT_APP_HASH}`;
+            
+            // const url = `https://gateway.marvel.com/v1/public/characters?limit=${requestLimit}&offset=${offset}&ts=${process.env.REACT_APP_TS}&apikey=${process.env.REACT_APP_PUBLIC_KEY}&hash=${process.env.REACT_APP_HASH}`;
+            
+            const res = await fetch(url);
+            const response = await res.json();
+            const data = response.data.results;
+            
+            return data;
+            
+        } catch(err) {
+            console.log('There was an error fetching data:::', err);
+        }
+        
     }
 
-    const {loading, error, persons, hasMore} = useSearchDb(query, offset);
+    const handleSearch = (event) => {
+        event.preventDefault();
+        loadData(event.target[0].value).then(data => setValue(data));
+    }
 
+    console.log(value);
+   
     return(
         <div className="search">
-            <input type="text" placeholder='Search' onChange={handleSearch}/>
-            <button type="submit"><BsSearch/></button>
+            <form onSubmit={handleSearch}>
+                <input label='search' type="text" placeholder='Search'/>
+                <button type='submit'><BsSearch/></button>
+            </form>
         </div>
     )
 }
--- src/routes/home/home.component.jsx
+++ src/routes/home/home.component.jsx
@@ -3,33 +3,9 @@ import { IdCard } from "../../components/id-card/id-card.component";
 import './home.styles.scss';
 
 export const Home = () => {
-
-    // const handleLoadData = () => {
-    //     loadData();
-    //     offset += 100;
-    // }
-
-    // const handleScroll = (e) => {
-    //     if(e.target.documentElement.scrollTop + window.innerHeight + 1 >= e.target.documentElement.scrollHeight) {
-    //         handleLoadData();
-    //     }
-    // }
-
-    // useEffect(() => {
-    //     handleLoadData();
-    //     window.addEventListener('scroll', handleScroll);
-    // }, [])
     
 
     return (
-        // <div className='home__container'>
-        //     {
-        //         persons.map( element => {
-        //             return <IdCard element={element} key={element.id}/>
-        //         })
-        //     }
-        // </div>
-
         null
     )
 }
