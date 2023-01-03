5f02fc6ee7cca833e668dc1011354cb92ac96aeb
Chamath Jayarathne <chamathsocial@gmail.com>
2023-01-02 20:17:22 -0500

    feat[spinner]: show spinner on initial load

---
+++ src/components/spinner/spinner.component.jsx
@@ -0,0 +1,7 @@
+import './spinner.styles.scss';
+
+export const Spinner = () => (
+    <div className="spinner-overlay">
+        <div className="spinner-container"></div>
+    </div>
+)
--- src/routes/home/home.component.jsx
+++ src/routes/home/home.component.jsx
@@ -1,17 +1,24 @@
-import { useContext } from "react";
+import { useEffect, useContext } from "react";
 
 import {SearchResultsContext} from '../../contexts/search-results.context';
 import { IdCard } from "../../components/id-card/id-card.component";
+import { Spinner } from '../../components/spinner/spinner.component';
+
+import {loadData} from '../../custom-functions/loadData';
 
 import './home.styles.scss';
 
 export const Home = () => {
-    const {searchResults} = useContext(SearchResultsContext);
+    const {searchResults, setSearchResults} = useContext(SearchResultsContext);
+
+    useEffect(() => {
+        loadData('').then(data => setSearchResults(data));
+    }, [])
 
     return (
  
         <div className="home__container">
-            {searchResults === null ? <div>Loading...</div> :
+            {searchResults === null ? <Spinner/> :
                 searchResults.map(element => <IdCard element={element} key={element.id}/>)
             }
         </div>
